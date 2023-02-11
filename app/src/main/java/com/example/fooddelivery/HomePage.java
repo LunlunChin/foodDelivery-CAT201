ee8bba06cf2aea99e1b6f905c7d745d3512d624f
Lun <zhielun999@gmail.com>
2023-02-11 23:17:28 +0800

    New Main UI

--- app/src/main/java/com/example/fooddelivery/HomePage.java
+++ app/src/main/java/com/example/fooddelivery/HomePage.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HomePage extends AppCompatActivity {
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://lunfood-251a1-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("category");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
